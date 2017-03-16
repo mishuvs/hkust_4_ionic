8c2a10ef770dc2e946cfc980bc36803516d75e71
vaibhav <robuxinc@gmail.com>
2017-03-16 22:20:58 +0530

    Add Week3 Excercise 3: Using Local Storage

--- Week3/conFusion/www/js/services.js
+++ Week3/conFusion/www/js/services.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('conFusion.services',['ngResource'])
-        .constant("baseURL","http://localhost:3000/")
+        .constant("baseURL","http://192.168.0.3:3000/")
         .factory('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
             return $resource(baseURL + "dishes/:id", null, {
@@ -56,5 +56,22 @@ angular.module('conFusion.services',['ngResource'])
 	.factory('promotionFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
             return $resource(baseURL + "promotions/:id");
 
+}])
+
+.factory('$localStorage', ['$window', function($window) {
+  return {
+    store: function(key, value) {
+      $window.localStorage[key] = value;
+    },
+    get: function(key, defaultValue) {
+      return $window.localStorage[key] || defaultValue;
+    },
+    storeObject: function(key, value) {
+      $window.localStorage[key] = JSON.stringify(value);
+    },
+    getObject: function(key,defaultValue) {
+      return JSON.parse($window.localStorage[key] || defaultValue);
+    }
+  }
 }])
 ;
